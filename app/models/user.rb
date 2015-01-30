efea26ab2b3dfbbf64341a2d8bc6b76efe9a0916
William Holt <wtholt27@gmail.com>
2015-01-30 12:44:24 -0500

    merge changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-<<<<<<< HEAD
-
-=======
-
-
-
   has_many :posts, through: :user_posts
   has_many :user_posts
   has_many :comments, through: :user_comments
   has_many :user_comments
->>>>>>> 3694e34d5051313f2f4be466d17d32bc7f6ad9a4
 
   NICK_OPTIONS = [
     "His winning smile",
